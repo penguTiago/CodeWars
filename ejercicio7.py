47163b7e827ab2939832ca0e286ee785eb571478
penguTiago <tiago.ramirez@penguin.academy>
2025-07-02 21:55:35 +0200

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,11 @@
+'''
+Escriba una función que reciba un string como parámetro y retorne el string, 
+pero con cada elemento que estuviese en mayúsculas reemplazado por "$". 
+Por ejemplo si el string es "Viva la Vida", entonces tu función 
+debe retornar "$iva la $ida".
+'''
+
+def reemplazo(string):
+    pass
+
+print(reemplazo('Viva la Vida'))
